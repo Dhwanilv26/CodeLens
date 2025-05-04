d70ac16aa3fcadba89782849e56468c87831120e
Dhwanilv26 <dhwanil2604@gmail.com>
2025-05-04 18:54:04 +0530

    enhance indexing by saving embeddings to the database and logging progress in github-loader.ts

--- src/lib/github-loader.ts
+++ src/lib/github-loader.ts
@@ -1,6 +1,7 @@
 import { GithubRepoLoader } from "@langchain/community/document_loaders/web/github";
 import { Document } from "@langchain/core/documents";
 import { aiSummarizeCode, generateEmbedding } from "./gemini";
+import { db } from "@/server/db";
 export const loadGithubRepo = async (
   githubUrl: string,
   githubToken?: string,
@@ -90,15 +91,51 @@ export const indexGithubRepo = async (
   const docs = await loadGithubRepo(githubUrl, githubToken);
 
   const allEmbeddings = await generateEmbeddings(docs);
+
+  await Promise.allSettled(
+    allEmbeddings.map(async (embedding, index) => {
+      console.log(`processing ${index + 1} of ${allEmbeddings.length}`);
+
+      if (!embedding) {
+        return;
+      }
+
+      const sourceCodeEmbedding = await db.sourceCodeEmbedding.create({
+        data: {
+          summary: embedding.summary,
+          sourceCode: embedding.sourceCode,
+          fileName: embedding.fileName,
+          projectId: projectId,
+        },
+      });
+
+      // as prisma does not support vector format , we need to do it manually by using raw query
+      // we need to convert the embedding to vector format and then insert it into the database
+      await db.$executeRaw`
+        UPDATE "SourceCodeEmbedding" 
+        SET "summaryEmbedding" = ${embedding.embedding}::vector
+        WHERE "id" = ${sourceCodeEmbedding.id}`;
+    }),
+  );
 };
 
+// async functions always return a promise
 const generateEmbeddings = async (docs: Document[]) => {
   // first it will generate summary of the document and then it will generate embeddings for the document
 
+  // returning the promise of all documents for the async function
+  // this will return an array of objects with summary, embedding, sourceCode and fileName
   return await Promise.all(
     docs.map(async (doc) => {
       const summary = await aiSummarizeCode(doc);
-      const embedding=await generateEmbedding(summary);
+      const embedding = await generateEmbedding(summary);
+
+      return {
+        summary,
+        embedding,
+        sourceCode: JSON.parse(JSON.stringify(doc.pageContent)),
+        fileName: doc.metadata.source,
+      };
     }),
   );
 };
