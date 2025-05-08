07b41d515d273bd4cae724e589ccdecf8ca245b2
Dhwanilv26 <dhwanil2604@gmail.com>
2025-05-08 17:34:47 +0530

    changed window component and wrapped it in useffect to avoid ssr errors

--- src/app/(protected)/dashboard/invite-button.tsx
+++ src/app/(protected)/dashboard/invite-button.tsx
@@ -8,12 +8,28 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import useProject from "@/hooks/use-project";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { toast } from "sonner";
 
 const InviteButton = () => {
   const { projectId } = useProject();
   const [open, setOpen] = useState(false);
+  const [inviteLink, setInviteLink] = useState("");
+
+  // window is available only in clients and not in servers
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && projectId) {
+      setInviteLink(`${window.location.origin}/join/${projectId}`);
+    }
+  }, [projectId]);
+
+  const copyToClipboard = () => {
+    if (inviteLink) {
+      navigator.clipboard.writeText(inviteLink);
+      toast.success("Link copied to clipboard");
+    }
+  };
 
   return (
     <>
@@ -26,17 +42,12 @@ const InviteButton = () => {
           <Input
             className="mt-4"
             readOnly
-            onClick={() => {
-              navigator.clipboard.writeText(
-                `${window?.location.origin}/join/${projectId}`,
-              );
-              toast.success("Link copied to clipboard");
-            }}
-            value={`${window.location.origin}/join/${projectId}`}
+            onClick={copyToClipboard}
+            value={inviteLink}
           />
         </DialogContent>
       </Dialog>
-      <Button size={"sm"} onClick={() => setOpen(true)}>
+      <Button size="sm" onClick={() => setOpen(true)}>
         Invite Members
       </Button>
     </>
